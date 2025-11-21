dc3418834c588f37d84fe7406362a31090842438
LocalAnomaly <92178121+Danielmg05@users.noreply.github.com>
2025-11-21 19:28:58 +0100

    Añade cambio de escenas en el menú

---
+++ client/src/scripts/globals.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var main_menu = preload("res://screens/main_menu/main.tscn")
+var menu_offline = preload("res://screens/menu_offline/main.tscn")
+var menu_online = preload("res://screens/menu_online/main.tscn")
+var lobby = preload("res://screens/lobby/main.tscn")
+var game_list = preload("res://screens/game_list/main.tscn")
+var create_game = preload("res://screens/create_game/main.tscn")
