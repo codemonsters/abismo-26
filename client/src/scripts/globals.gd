c69c5ebb9b51c03f8139d79016600c9038fcad2c
asexto <a.sexto@gmail.com>
2025-12-05 18:58:41 +0100

    Reorganiza menús

--- client/src/scripts/globals.gd
+++ client/src/scripts/globals.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var main_menu = preload("res://screens/main_menu/main.tscn")
-var menu_offline = preload("res://screens/menu_offline/main.tscn")
-var menu_online = preload("res://screens/menu_online/main.tscn")
-var lobby = preload("res://screens/lobby/main.tscn")
-var game_list = preload("res://screens/game_list/main.tscn")
-var create_game = preload("res://screens/create_game/main.tscn")
+var scn_main_menu = preload("res://screens/main_menu/main.tscn")
+var scn_intro = preload("res://screens/intro/main.tscn")
+var scn_menu_online = preload("res://screens/menu_online/main.tscn")
+var scn_lobby = preload("res://screens/lobby/main.tscn")
+var scn_game_list = preload("res://screens/game_list/main.tscn")
+var scn_create_game = preload("res://screens/create_game/main.tscn")
+var scn_game = preload("res://screens/game/main.tscn")
