7b8b743be20b8f1a3408813b608bc6637576d481
asexto <a.sexto@gmail.com>
2026-02-06 19:27:21 +0100

    Incluye en la escena game todas las escenas de los estados del juego

--- client/src/screens/game/main.gd
+++ client/src/screens/game/main.gd
@@ -1,57 +1,31 @@
 extends Node3D
 
-var next_scene
-@onready var camera = $Environment/Camera3D
 signal level_passed
-var level_index = 0
-var levels = []
 
-var gamestate_scenes = {
-	"game_over": preload("res://screens/game/states/game_over/main.tscn"),
-	"ingame_menu": preload("res://screens/game/states/ingame_menu/main.tscn"),
-	"level_complete": preload("res://screens/game/states/level_complete/main.tscn"),
-	"level_intro": preload("res://screens/game/states/level_intro/main.tscn"),
-	"level_loading": preload("res://screens/game/states/level_loading/main.tscn"),
-	"level_transition": preload("res://screens/game/states/level_transition/main.tscn"),
-	"playing": preload("res://screens/game/states/playing/main.tscn")
-}
-
-func set_state(state_name):
-	var state_scene = get_node_or_null("state_scene")
-	if state_scene != null:
-		state_scene.exit()
-		remove_child(state_scene)
-		# TODO: ejecutar state_scene.queue_free()???
-	
-	state_scene = gamestate_scenes[state_name].instantiate()
-	state_scene.name = "state_scene"
-	add_child(state_scene)
-	state_scene.enter()
-
+@onready var camera = $Environment/Camera3D
+var current_level_index = 0
+var levels = []	# lista con los paths de las escenas que usamos como niveles
+var next_scene
 
 func _ready():
+	for state in $States.get_children():
+		state.disable()
 	for level_number in range(1, 5):
 		var number = "0" + str(level_number)
 		var level_path = "res://screens/game/levels/level" + number + "/main.tscn"
 		levels.append(level_path)
-	var current_level = load(levels[level_index])
+	var current_level = load(levels[current_level_index])
 	var current_scene = current_level.instantiate()
 	$CurrentLevel.add_child(current_scene)
 	host()
-	set_state("level_loading")
+	$States/LevelLoading.enable()
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("space"):
 		level_passed.emit()
-	$state_scene.process(delta)
 
 
-func physics_process(delta):
-	$"state_scene".physics_process(delta)
-	pass
-	
-	
 func host():
 	$"UI/Network Popup/LAN Bootstrapper".host()
 
@@ -59,7 +33,7 @@ func host():
 func _on_level_passed() -> void:
 	for child in $CurrentLevel.get_children():
 		child.queue_free()
-	level_index += 1
-	var current_level = load(levels[level_index])
+	current_level_index += 1
+	var current_level = load(levels[current_level_index])
 	var current_scene = current_level.instantiate()
 	$CurrentLevel.add_child(current_scene)
