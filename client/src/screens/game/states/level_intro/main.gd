853309d1828fb55a4e08187d604964d6108cf3d4
asexto <a.sexto@gmail.com>
2026-02-20 19:04:53 +0100

    Sustituye tween por AnimationPlayer en la escena level_intro

--- client/src/screens/game/states/level_intro/main.gd
+++ client/src/screens/game/states/level_intro/main.gd
@@ -1,24 +1,23 @@
 extends "res://screens/game/states/game_state.gd"
 class_name LevelIntroState
 
+func _ready():
+	$IntroTip.visible = false
+
+	
 func enter():
 	print("Entering Level Intro")
 	$IntroTip.set_position(Vector2(0, -40))
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property($IntroTip, "position", Vector2(0, 648), 3.0)
-	tween.tween_property($IntroTip, "modulate", Color(1, 1, 1, 0), 3.0)
-	tween.finished.connect(_on_tween_finished)
+	$AnimationPlayer.play("Enter")
+
 
 func exit():
 	print("Exiting Level Intro")
 
+
 func _process(delta):
 	pass
 
+
 func _physics_process(delta):
 	pass
-
-func _on_tween_finished():
-	$"../Playing".enable()
-	disable()
