47bc71954bba7a073542cfcf671673164ece7058
Happy-reindeer007 <114765327+Happy-reindeer007@users.noreply.github.com>
2026-03-06 18:47:31 +0100

    pinchos añadidos

--- client/src/screens/game/levels/level01/bunny.gd
+++ client/src/screens/game/levels/level01/bunny.gd
@@ -2,11 +2,3 @@ extends Node3D
 
 func _ready():
 	position = Vector3(0, 1, 13)
-
-func _rollback_tick(delta, _tick, _is_fresh):
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	var input_dir = input.movement
-	var direction = (transform.basis * Vector3(0, 0, input_dir.z)).normalized()
-	$blockbench_export/AnimationPlayer.play("jumping_walking")
