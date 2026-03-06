040b7c77598b0dfbf3afdd4a0f1556227f21ff92
pistacho29 <>
2026-03-06 18:56:58 +0100

    conejo (no funciona)

--- client/src/screens/game/levels/level01/bunny.gd
+++ client/src/screens/game/levels/level01/bunny.gd
@@ -1,12 +1,19 @@
 extends Node3D
+extends CharacterBody3D
 
 func _ready():
 	position = Vector3(0, 1, 13)
 
 func _rollback_tick(delta, _tick, _is_fresh):
+	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	var input_dir = input.movement
-	var direction = (transform.basis * Vector3(0, 0, input_dir.z)).normalized()
-	$blockbench_export/AnimationPlayer.play("jumping_walking")
+	if velocity.z < 0:
+		$blockbench_export.rotation_degrees.y = 0
+	elif velocity.z > 0:
+		$blockbench_export.rotation_degrees.y = -180
+		
+	if is_on_floor():
+			$blockbench_export/AnimationPlayer.play("jumping_walking")
+			velocity.y = 3.5
