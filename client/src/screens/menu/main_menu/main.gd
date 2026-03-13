a6a373f1ab9fdec48fbd0808419eb1e209542a34
asexto <a.sexto@gmail.com>
2026-03-13 18:44:16 +0100

    Mueve escenas de menús a nueva carpeta y renombra escenas globales

--- client/src/screens/menu/main_menu/main.gd
+++ client/src/screens/menu/main_menu/main.gd
@@ -9,20 +9,20 @@ func _ready() -> void:
 
 
 func _on_play_offline_pressed() -> void:
-	var game = Globals.scn_game.instantiate()
-	var intro = Globals.scn_intro.instantiate()
+	var game = Globals.screen_game.instantiate()
+	var intro = Globals.screen_intro.instantiate()
 	game.next_scene = self
 	intro.next_scene = game
 	get_parent().change_screen(intro)
 
 
 func _on_play_online_pressed() -> void:
-	var online_menu = Globals.scn_menu_online.instantiate()
+	var online_menu = Globals.screen_menu_online.instantiate()
 	online_menu.prev_scene = self
 	get_parent().change_screen(online_menu)
 
 
 func _on_timer_intro_timeout() -> void:
-	var intro = Globals.scn_intro.instantiate()
+	var intro = Globals.screen_intro.instantiate()
 	intro.next_scene = self
 	get_parent().change_screen(intro)
--- client/src/scripts/globals.gd
+++ client/src/scripts/globals.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var scn_main_menu = preload("res://screens/main_menu/main.tscn")
-var scn_intro = preload("res://screens/intro/main.tscn")
-var scn_menu_online = preload("res://screens/menu_online/main.tscn")
-var scn_lobby = preload("res://screens/lobby/main.tscn")
-var scn_game_list = preload("res://screens/game_list/main.tscn")
-var scn_create_game = preload("res://screens/create_game/main.tscn")
-var scn_game = preload("res://screens/game/main.tscn")
+var screen_intro = preload("res://screens/intro/main.tscn")
+var screen_menu_main = preload("res://screens/menu/main_menu/main.tscn")
+var screen_menu_online = preload("res://screens/menu/menu_online/main.tscn")
+var screen_menu_lobby = preload("res://screens/menu/lobby/main.tscn")
+var screen_menu_game_list = preload("res://screens/menu/game_list/main.tscn")
+var screen_menu_create_game = preload("res://screens/menu/create_game/main.tscn")
+var screen_game = preload("res://screens/game/main.tscn")
