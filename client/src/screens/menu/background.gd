b1229f5512a5808611011e24de1bd64fe366818f
asexto <a.sexto@gmail.com>
2026-03-20 18:58:55 +0100

    Simplificado efecto de agua en la pantalla principal

--- client/src/screens/menu/background.gd
+++ client/src/screens/menu/background.gd
@@ -1,5 +1,5 @@
 extends Node3D
 
 '''
-Efecto de agua "Simple Water in Godot - Godot 4 Tutorial" (autor: Omogonix): https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1
+Efecto de agua "Simple Water in Godot - Godot 4 Tutorial" (autor: Omogonix): https://www.youtube.com/watch?v=Ej_RyOG3d0c
 '''
