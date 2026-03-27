80b280a684b0bbfce9bafad48cfc418fea4be0e0
asexto <a.sexto@gmail.com>
2026-03-27 19:20:37 +0100

    Revert "todo jodido culpa de telmo"
    This reverts commit 13bf999c26bc81af2883b32717b4c55bfda6b0da, reversing
    changes made to f43170a9e6db3ebdb6d7fa8938f890b469087f39.

--- client/src/screens/menu/main_menu/main.gd
+++ client/src/screens/menu/main_menu/main.gd
@@ -6,7 +6,7 @@ signal change_screen_requested(scene, next_scene)
 func _on_play_offline_pressed() -> void:
 	var game = Globals.screen_game.instantiate()
 	var intro = Globals.screen_intro.instantiate()
-	game.next_scene = get_parent().get_parent()
+	game.next_scene = get_parent()
 	intro.next_scene = game
 	emit_signal("change_screen_requested", intro, null)
 
@@ -19,5 +19,5 @@ func _on_play_online_pressed() -> void:
 
 func _on_timer_intro_timeout() -> void:
 	var intro = Globals.screen_intro.instantiate()
-	intro.next_scene = get_parent().get_parent()
+	intro.next_scene = get_parent()
 	emit_signal("change_screen_requested", intro, null)
