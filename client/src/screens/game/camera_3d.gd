7579e21df1a50dc6b002b9ff4789e69723fa7695
Happy-reindeer007 <alexfreirefdez@gmail.com>
2026-04-10 19:33:52 +0200

    Cambia el ángulo de la cámara

--- client/src/screens/game/camera_3d.gd
+++ client/src/screens/game/camera_3d.gd
@@ -10,11 +10,12 @@ func _physics_process(delta):
 		# Calculamos dónde debería estar la cámara
 		var target_pos = target.global_position + offset
 		target_pos.x = offset.x
-		target_pos.y = 3
+		target_pos.y = 15
 		# hace que la camara no se mueva llegado el final del mundo
 		if target_pos.z <= -map_length/2 + 9.5:
 			target_pos.z = -14.5
 		elif target_pos.z >= map_length/2 - 9:
 			target_pos.z = 16.75
+		rotation_degrees.x = -30
 		# Movemos la cámara suavemente hacia esa posición usando interpolación lineal (Lerp)
 		global_position = global_position.lerp(target_pos, smooth_speed * delta)
