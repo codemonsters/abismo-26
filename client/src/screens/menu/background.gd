f48f7a05c97169982b58f9a1a5cbb2a696bdd2a0
asexto <a.sexto@gmail.com>
2026-04-17 19:33:41 +0200

    Simplifica efecto de agua del menú con textura y normal map pregenerados

--- client/src/screens/menu/background.gd
+++ client/src/screens/menu/background.gd
@@ -3,3 +3,15 @@ extends Node3D
 '''
 Efecto de agua "Simple Water in Godot - Godot 4 Tutorial" (autor: Omogonix): https://www.youtube.com/watch?v=Ej_RyOG3d0c
 '''
+
+'''
+func _ready():
+	var mat = $Water/MeshInstance3D.get_active_material(0)
+	var noise_tex: NoiseTexture2D = mat.albedo_texture
+
+	if noise_tex:
+		await get_tree().process_frame  # asegurar que se ha generado
+
+		var img = noise_tex.get_image()
+		img.save_png("res://screens/menu/water_texture_baked.png")
+'''
