c6826fa00b1d8461da3560ecb9341d86d43d4153
asexto <a.sexto@gmail.com>
2026-05-29 18:40:46 +0200

    Arreglado change_screen

--- client/src/main.gd
+++ client/src/main.gd
@@ -6,15 +6,9 @@ func _ready() -> void:
 
 func change_screen(scene, next_scene = null):
 	if get_node_or_null("current_screen") != null: remove_child(get_node("current_screen"))
-#<<<<<<< Updated upstream
 	scene.name = "current_screen"
 	if scene.has_signal("change_screen_requested"):
 		scene.change_screen_requested.connect(change_screen) #TODO: probar for child in get_tree()
-#=======
-#	scenename = "current_screen"
-	print(scene.get_script())
-	scene.change_screen_requested.connect(change_screen)
-#>>>>>>> Stashed changes
 	if next_scene:
 		scene.next_scene = next_scene
 	add_child(scene)
