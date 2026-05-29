6680867f1eeab2c2aa2ca7aad1146c4e3fb5c72a
pistacho29 <omlet.miraz@gmail.com>
2026-05-29 19:38:10 +0200

    progreso barra vida

--- client/src/screens/game/progress_bar.gd
+++ client/src/screens/game/progress_bar.gd
@@ -4,3 +4,10 @@ var vida_inicial: float = 100
 
 func _ready() -> void:
 	value = vida_inicial
+
+func Recibir_daño(Daño : float):
+	if value > 0:
+		value -= Daño
+		if value <= 0:
+			pass #programar muerte
+	
